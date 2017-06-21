5c3004820dba02e7b9242142a6662a7c20492bb9
Sahana Murthy <s.murthy730.com>
2017-06-21 14:15:21 -0700

    Completely fixed the image url bug

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -10,10 +10,11 @@ class Movie < ApplicationRecord
 
   def image_url
     orig_value = read_attribute :image_url
-    if !orig_value
+    if !orig_value || orig_value == "http://lorempixel.com/185/278/"
       MovieWrapper::DEFAULT_IMG_URL
     else
-      orig_value
+      orig_value.slice!("https://image.tmdb.org/t/p/w185")
+      MovieWrapper.construct_image_url(orig_value)
     end
   end
 end
